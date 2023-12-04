38bb8d374eaa0faab37bcff2b695873bff7b24de
sea-shunned <seashunned@gmail.com>
2023-12-04 15:29:46 +0000

    Refactor tests to use utils load func

--- tests/test_jsons.py
+++ tests/test_jsons.py
@@ -4,16 +4,10 @@ from pathlib import Path
 from pydantic import ValidationError
 import pytest
 
-from aiod_registry import ModelManifest
+from aiod_registry import ModelManifest, get_manifest_paths
 
 
-def get_jsons():
-    json_dir = Path(__file__).parent.parent / "aiod_registry" / "manifests"
-    print(json_dir)
-    return json_dir.glob("*.json")
-
-
-@pytest.mark.parametrize("json_path", get_jsons())
+@pytest.mark.parametrize("json_path", get_manifest_paths())
 def test_manifest(json_path):
     with open(json_path, "r") as f:
         json_manifest = json.load(f)
