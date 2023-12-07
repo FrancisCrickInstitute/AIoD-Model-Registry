bd21de50562ef05dcef7ad6bd711d993031592d0
sea-shunned <seashunned@gmail.com>
2023-12-07 16:25:16 +0000

    Abstract schema fields, embed task-level params, dictify, determine location type, abstract task names

--- aiod_registry/__init__.py
+++ aiod_registry/__init__.py
@@ -1,2 +1,2 @@
-from aiod_registry.schema import ModelManifest
+from aiod_registry.schema import ModelManifest, TASK_NAMES
 from aiod_registry.utils import get_manifest_paths, load_manifests
--- aiod_registry/schema.py
+++ aiod_registry/schema.py
@@ -1,8 +1,25 @@
-import json
 from pathlib import Path
 from typing import Optional, Union
+from urllib.parse import urlparse
 
 from pydantic import BaseModel, ConfigDict, Field, model_validator, AnyUrl
+from typing_extensions import Annotated
+
+TASK_NAMES = {
+    "mito": "Mitochondria",
+    "er": "Endoplasmic Reticulum",
+    "ne": "Nuclear Envelope",
+    "everything": "Everything!",
+}
+task_names = "|".join(TASK_NAMES.keys())
+
+# Define custom types/fields
+# Centralise to make it easier to change later
+# Regex pattern to match task names, ignoring case
+Task = Annotated[str, Field(..., pattern=rf"^(?i:{task_names})$")]
+ModelName = Annotated[str, Field(..., min_length=1, max_length=50)]
+ParamName = Annotated[str, Field(..., min_length=1, max_length=50)]
+ParamValue = Union[str, int, float, bool, list[Union[str, int, float, bool]]]
 
 
 def shorten_name(name: str) -> str:
@@ -13,25 +30,10 @@ class StrictModel(BaseModel):
     model_config = ConfigDict(extra="forbid")
 
 
-class ModelVersionTask(StrictModel):
-    # Regex pattern to match task names, ignoring case
-    task: str = Field(..., pattern=r"^(?i:mito|er|ne|everything)$")
-    location: Union[Path, AnyUrl, str] = Field(
-        ...,
-        description="Either a url or a filepath (will be skipped if the path does not exist/cannot be read)",
-    )
-    config_path: Optional[Union[Path, str]] = None
-
-
-class ModelVersion(StrictModel):
-    name: str = Field(..., min_length=1, max_length=50)
-    tasks: list[ModelVersionTask]
-
-
 class ModelParam(StrictModel):
-    name: str = Field(..., min_length=1, max_length=50)
+    name: ParamName
     short_name: Optional[str] = None
-    value: Union[str, int, float, bool, list[Union[str, int, float, bool]]]
+    value: ParamValue
     tooltip: Optional[str] = None
 
     @model_validator(mode="after")
@@ -41,10 +43,42 @@ class ModelParam(StrictModel):
         return self
 
 
+class ModelVersionTask(StrictModel):
+    location: str = Field(
+        ...,
+        description="Either a url or a filepath (will be skipped if the path does not exist/cannot be read!)",
+    )
+    config_path: Optional[Union[Path, str]] = None
+    params: Optional[list[ModelParam]] = None
+    location_type: Optional[str] = None
+
+    @model_validator(mode="after")
+    def get_location_type(self):
+        # Skip if provided
+        if self.location_type is not None:
+            return self
+        # Otherwise, determine the type
+        res = urlparse(self.location)
+        if res.scheme in ("http", "https"):
+            self.location_type = "url"
+        elif res.scheme in ("file", ""):
+            self.location_type = "file"
+        else:
+            # NOTE: Because of including "" above, it is unlikely this will be reached
+            raise TypeError(
+                f"Cannot determine type (file/url) of location: {self.location}!"
+            )
+        return self
+
+
+class ModelVersion(StrictModel):
+    tasks: dict[Task, ModelVersionTask]
+
+
 class ModelManifest(StrictModel):
     name: str = Field(..., min_length=1, max_length=50)
     short_name: Optional[str] = None
-    versions: list[ModelVersion]
+    versions: dict[ModelName, ModelVersion]
     params: Optional[list[ModelParam]] = None
     config: Optional[Path] = None
 
@@ -53,3 +87,11 @@ class ModelManifest(StrictModel):
         if self.short_name is None:
             self.short_name = shorten_name(self.name)
         return self
+
+    # Embed base model params into each version if not provided
+    @model_validator(mode="after")
+    def fill_empty_params(self):
+        for version in self.versions.values():
+            for task in version.tasks.values():
+                if task.params is None:
+                    task.params = self.params
