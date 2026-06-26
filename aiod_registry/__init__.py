2f6a5e83d7fc07452cb0c808883d01c8b14ccd7c
sea-shunned <seashunned@gmail.com>
2026-06-26 15:13:31 +0100

    Add dynamic __version__

--- aiod_registry/__init__.py
+++ aiod_registry/__init__.py
@@ -1,3 +1,10 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("aiod-registry")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 from aiod_registry.schema import TASK_NAMES, ModelManifest
 from aiod_registry.utils import get_manifest_paths, load_manifests
 
